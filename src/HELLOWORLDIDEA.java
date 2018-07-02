9581c803397c31b07669bcaca98c565890e6feb1
Maciej <m.czajczynski28@gmail.com>
2018-07-02 19:04:34 +0200

    KONFIGURACJA GIT

---
+++ src/HELLOWORLDIDEA.java
@@ -0,0 +1,12 @@
+public class HELLOWORLDIDEA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+//        System.out.println("Hello3");
+//        System.out.println("Hello4");
+//        System.out.println("Hello5");
+        System.out.println("Hello6");
+
+    }
+}
